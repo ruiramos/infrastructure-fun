fdaa4c3d0ecc3d7e680529b1ca82aa116112930c
Rui Ramos <ruiramos@gmail.com>
2021-01-05 18:48:57 +0000

    Makes data-storage use redis

--- services/data-storage-service/src/main.rs
+++ services/data-storage-service/src/main.rs
@@ -1,42 +1,32 @@
 use std::env;
 use tide::Request;
 use tide::convert::json;
-use std::collections::HashMap;
-use std::sync::{Arc, Mutex};
+extern crate redis;
+use redis::Commands;
 
 use data_storage_service::encrypt::{encrypt_data, decrypt_data};
 
-#[derive(Clone, Default, Debug)]
-struct State {
-    map: Arc<Mutex<HashMap<String, String>>>
-}
-impl State {
-    fn new() -> Self {
-        State {
-            map: Arc::new(Mutex::new(HashMap::new()))
-        }
-    }
-}
-
 #[async_std::main]
 async fn main() -> tide::Result<()> {
+    tide::log::start();
     let port = env::var("PORT").unwrap_or_else(|_| "8088".to_string());
 
-    let mut app = tide::with_state(State::new());
+    let mut app = tide::new();
 
+    app.with(tide::log::LogMiddleware::new());
     app.at("/").post(store_data);
     app.at("/:id").post(get_data);
     app.at("/healthz").get(say_ok);
-
-    println!("* Starting the server running on port {}", port);
 
     app.listen(format!("0.0.0.0:{}", port)).await?;
     Ok(())
 }
 
-async fn store_data(mut req: Request<State>) -> tide::Result {
+async fn store_data(mut req: Request<()>) -> tide::Result {
+    let client = get_redis_client()?;
+    let mut con = client.get_connection()?;
+
     let data = req.body_string().await?;
-    let state: &State = req.state();
 
     if data.is_empty() {
         return Err(tide::Error::from_str(tide::http::StatusCode::BadRequest, "Missing data on POST body"));
@@ -44,21 +34,21 @@ async fn store_data(mut req: Request<State>) -> tide::Result {
 
     let (hash, password, encrypted) = encrypt_data(data);
 
-    let mut map = state.map.lock().unwrap();
-    map.insert(hash.clone(), encrypted);
+    let _ : () = con.set(hash.clone(), encrypted)?;
 
     let prefix_url = env::var("SERVICE_URL").unwrap_or_else(|_| "http://localhost:8088/".to_string());
 
     Ok(json!({"url": format!("{}{}", prefix_url, hash), "password": password}).into())
 }
 
-async fn get_data(mut req: Request<State>) -> tide::Result {
+async fn get_data(mut req: Request<()>) -> tide::Result {
+    let client = redis::Client::open("redis://127.0.0.1/")?;
+    let mut con = client.get_connection()?;
     let password = req.body_string().await?;
-    let hash = req.param("id")?;
-    let state: &State = req.state();
 
-    let map = state.map.lock().unwrap();
-    let value = map.get(hash);
+    let hash = req.param("id")?;
+
+    let value: Option<String> = con.get(hash).ok();
 
     match value {
         None => Err(tide::Error::from_str(tide::http::StatusCode::Forbidden, "Forbidden")),
@@ -72,6 +62,15 @@ async fn get_data(mut req: Request<State>) -> tide::Result {
     }
 }
 
-async fn say_ok(mut _req: Request<State>) -> tide::Result {
+async fn say_ok(mut _req: Request<()>) -> tide::Result {
     Ok("ok".into())
 }
+
+fn get_redis_host() -> String {
+    env::var("REDIS_URL").unwrap_or_else(|_| "127.0.0.1/".to_string())
+}
+
+fn get_redis_client() -> Result<redis::Client, redis::RedisError> {
+    redis::Client::open(format!("redis://{}", get_redis_host()))
+}
+
